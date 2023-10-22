83bf260195f3847c477727b23646ebefc53ebc93
tnatna0801 <min120926@gmail.com>
2023-10-22 14:31:14 +0900

    [BOJ] 1389. 케빈 베이컨 6단계

--- soomin_kim/BOJ_1389.java
+++ soomin_kim/BOJ_1389.java
@@ -13,11 +13,13 @@ public class BOJ_1389 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
+        // 입력
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         min = Integer.MAX_VALUE;
         result = 0;
 
+        // 인접행렬 생성
         int[][] adj = new int[n+1][n+1];
         for(int i = 0; i<m; i++){
             st = new StringTokenizer(br.readLine());
@@ -29,6 +31,7 @@ public class BOJ_1389 {
             adj[to][from] = 1;
         }
 
+        // 각 정점을 기준으로 bfs 탐색한다.
         for(int i = 1; i<=n; i++){
                 bfs(i, n, adj);
         }
@@ -42,7 +45,7 @@ public class BOJ_1389 {
         int count = 0;
 
         q.add(new int[] {index, 0});
-        visited[index] = true;
+        visited[index] = true; // 시작 정점 방문처리
 
         while(!q.isEmpty()){
             int[] now = q.poll();
@@ -50,7 +53,7 @@ public class BOJ_1389 {
 
             for(int i = 1; i<=n; i++){
                 if(adj[now[0]][i] == 1 && !visited[i]){
-                    q.add(new int[] {i, now[1]+1});
+                    q.add(new int[] {i, now[1]+1}); // 인접하고 방문하지 않은 정점을 q에 넣음
                     visited[i] = true;
                 }
             }
